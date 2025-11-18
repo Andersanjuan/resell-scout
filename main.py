f21f7ed68b0ae91ec2fbfecb26ca55e9fe259360
Ander <andersanjuan@gmail.com>
2025-11-18 13:04:25 -0500

    User requests, GitHUB API via menu

--- main.py
+++ main.py
@@ -5,11 +5,37 @@ Entry point for the AI Resell Scout project.
 For now, this only shows a simple menu and exits.
 Later, we will add real functionality here.
 """
+import requests
 
 def show_menu():
     print("=== AI Resell Scout ===") # Simple menu display
-    print("1. Test internet connection")
+    print("1. Test internet connection - github API")
     print("2. Exit")
+
+
+def test_internet_connection():
+    """
+    Test internet connection by making a request to GitHub API.
+    """    
+    url= "https://api.github.com"
+    
+    try:
+        response = requests.get(url, timeout=10)
+        print(f"Internet connection is working. Status Code:")
+        print(response.status_code)
+
+        if response.status_code == 200:
+            data = response.json() #JSON to Python dict
+            print("Response JSON data:")
+            for key in data.keys():
+                print(f"   - {key}")
+        else:
+            print("Failed to retrieve data from GitHub API.")
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+
+    print()  # Blank line for better readability
+
 
 def main():
     while True:
@@ -17,8 +43,7 @@ def main():
         choice = input("Choose an option (1-2): ").strip()
 
         if choice == "1":
-            print("This is where we will later test a real request.")
-            print("For now, this is just a placeholder.\n")
+            test_internet_connection()
         elif choice == "2":
             print("Goodbye.")
             break
